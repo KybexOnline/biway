5ecde45619b122a1de5587a8c5bce860cc4b9fb2
Arsalan <am1r.ar3alan@gmail.com>
2026-06-29 18:40:57 +0330

    add static serve for serve admin panel

--- internal/admin/api/api.go
+++ internal/admin/api/api.go
@@ -1,14 +1,23 @@
 package api
 
 import (
+	"embed"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/KybexOnline/biway/internal/config"
 	"github.com/KybexOnline/biway/pkg/middlewares"
 	"github.com/KybexOnline/biway/pkg/utils"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 )
+
+//go:embed static_files
+var embeddedFiles embed.FS
 
 func InitAdminRouter() *gin.Engine {
 
@@ -16,6 +25,7 @@ func InitAdminRouter() *gin.Engine {
 
 	if config.AppConfig.Environment == "production" {
 		gin.SetMode(gin.ReleaseMode)
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
 
 	engine := gin.New()
@@ -34,6 +44,22 @@ func InitAdminRouter() *gin.Engine {
 
 	engine.Use(gin.Recovery())
 
+	distFS := getFileSystem("static_files")
+	engine.Use(static.Serve("/", distFS))
+
+	engine.NoRoute(func(c *gin.Context) {
+		// Only serve index.html for non-API routes
+		if !strings.HasPrefix(c.Request.RequestURI, "/api") {
+			index, err := distFS.Open("index.html")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer index.Close()
+			stat, _ := index.Stat()
+			http.ServeContent(c.Writer, c.Request, "index.html", stat.ModTime(), index)
+		}
+	})
+
 	api := engine.Group("/api/v1")
 	{
 		api.GET("/health", func(ctx *gin.Context) {
@@ -50,4 +76,12 @@ func InitAdminRouter() *gin.Engine {
 	}
 
 	return engine
+}
+
+func getFileSystem(path string) static.ServeFileSystem {
+	fs, err := static.EmbedFolder(embeddedFiles, path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fs
 }
